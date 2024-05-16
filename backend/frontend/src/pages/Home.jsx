f972b143670a41ddef75c369fb6950cc01f677c0
RuiPires999 <ruipedro.pires@hotmail.com>
2024-05-16 10:22:02 +0100

    feat: Add file upload functionality to registration page

--- backend/frontend/src/pages/Home.jsx
+++ backend/frontend/src/pages/Home.jsx
@@ -133,127 +133,177 @@ import { useNavigate } from 'react-router-dom';
 
 
 function Home() {
-  const [avaliacoes, setAvaliacoes] = useState([]);
-  const [userIsAuthorized, setUserIsAuthorized] = useState(false);
-  const username = localStorage.getItem('username');    useEffect(() => {
-      api.get('/api/check_group/')
-          .then(response => {
-              if (response.data.is_superuser) {
-                  setUserIsAuthorized(true);
-              }
-          })
-          .catch(error => {
-              console.error('There was an error!', error);
-          });
-  }, []);    useEffect(() => {
-      api.get('/api/user/avaliacoes/')
-          .then(response => {
-              setAvaliacoes(response.data);
-          })
-          .catch(error => {
-              console.error('There was an error!', error);
-          });
-  }, []);    // Group avaliacoes by avaliador
-  const avaliadores = avaliacoes.reduce((groups, avaliacao) => {
-      const key = avaliacao.avaliador;
-      if (!groups[key]) {
-          groups[key] = [];
-      }
-      groups[key].push(avaliacao);
-      return groups;
-  }, {});    
-  if (!userIsAuthorized) {
-      console.log("notautorized");
-      const avaliadorNumber = username.split('_').pop();        return (
-          <Table striped bordered hover className="shadow">
-              <thead className="thead-dark">
-                  <tr>
-                      <th>Avaliador</th>
-                      <th>Avaliados</th>
-                      <th>Data Inicial</th>
-                      <th>Data Final</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  {Object.entries(avaliadores)
-                      .filter(([avaliador]) => avaliador === avaliadorNumber)  // Only include rows where the current user is the avaliador
-                      .map(([avaliador, avaliacoes], index) => (
-                          avaliacoes.map((avaliacao, subIndex) => (
-                              <tr key={`${index}-${subIndex}`}>
-                                  <td>{avaliador}</td>
-                                  <td>{avaliacao.avaliado}</td>
-                                  <td>{avaliacao.data_inicial}</td>
-                                  <td>{avaliacao.data_final}</td>
-                              </tr>
-                          ))
-                      ))}
-              </tbody>
-          </Table>
-      );
-  }    const handleDelete = async (avaliadorToDelete, avaliadoToDelete) => {
-      const newAvaliacoes = avaliacoes.filter(avaliacao => !(avaliacao.avaliador === avaliadorToDelete && avaliacao.avaliado === avaliadoToDelete));
-      setAvaliacoes(newAvaliacoes);
-      try {
-          await api.delete(`/api/user/delete/${avaliadorToDelete}/${avaliadoToDelete}/`);
-          window.location.reload();
-      } catch (error) {
-          alert(error);
-      }
-  };    return (        
-        
+    const [avaliacoes, setAvaliacoes] = useState([]);
+    const [userIsAuthorized, setUserIsAuthorized] = useState(false);
+    const username = localStorage.getItem('username');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get('/api/check_group/')
+            .then(response => {
+                if (response.data.is_superuser) {
+                    setUserIsAuthorized(true);
+                }
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
+
+    useEffect(() => {
+        api.get('/api/user/avaliacoes/')
+            .then(response => {
+                setAvaliacoes(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
+
+    // Group avaliacoes by avaliador
+    const avaliadores = avaliacoes.reduce((groups, avaliacao) => {
+        const key = avaliacao.avaliador;
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+        groups[key].push(avaliacao);
+        return groups;
+    }, {});
+
+    if (!userIsAuthorized) {
+        const avaliadorNumber = username.split('_').pop()
+
+        return (
+            < Container fluid className="container-padding-top" >
+                <header className="header">
+                    <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/eab3563f117d37a0aba7d9f1e0437d07e9641ada43a08ad0c0e29d55a22d1107?apiKey=d4ef87dbdadc4d0c82074c04dc070338&"
+                        alt="Company Logo"
+                        className="logo"
+                    />
+                    <nav className="navigation">
+                        <a href="/logout" className="nav-link bordered" style={{ backgroundColor: 'black', color: 'white' }}>
+                            Logout
+                        </a>
+                    </nav>
+                </header>
+                <Row>
+                    <Col md={2} className="sidebar bg-light">
+                        {/* Sidebar content */}
+                    </Col>
+                    <Col md={10}>
+                        <h1 className="text-center mb-4 heading-margin-top">Avaliações</h1>
+                        <Table striped bordered hover className="shadow" style={{ margin: '10px', padding: '20px' }}>
+                            <thead className="thead-dark">
+                                <tr>
+                                    <th style={{ margin: '100px', padding: '10px 20px' }}>Avaliador</th>
+                                    <th style={{ margin: '100px', padding: '10px 20px' }}>Avaliados</th>
+                                    <th style={{ margin: '100px', padding: '10px 20px' }}>Data Inicial</th>
+                                    <th style={{ margin: '100px', padding: '10px 20px' }}>Data Final</th>
+                                    <th style={{ margin: '100px', padding: '10px 20px' }}>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Object.entries(avaliadores)
+                                    .filter(([avaliador]) => avaliador === avaliadorNumber)  // Only include rows where the current user is the avaliador
+                                    .map(([avaliador, avaliacoes], index) => (
+                                        avaliacoes.map((avaliacao, subIndex) => (
+                                            <tr key={`${index}-${subIndex}`}>
+                                                <td>{avaliador}</td>
+                                                <td>{avaliacao.avaliado}</td>
+                                                <td>{avaliacao.data_inicial}</td>
+                                                <td>{avaliacao.data_final}</td>
+                                                <td>
+                                                    <button onClick={() => navigate('/evaluationForm')} style={{
+                                                        background: '#000000',
+                                                        color: 'white',
+                                                        border: 'none',
+                                                        padding: '10px 20px',
+                                                        margin: '2px',
+                                                        cursor: 'pointer',
+                                                        borderRadius: '5px',
+                                                    }}>Ficha</button>
+
+                                                </td>
+                                            </tr>
+                                        ))
+                                    ))}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+
+    const handleDelete = async (avaliadorToDelete, avaliadoToDelete) => {
+        const newAvaliacoes = avaliacoes.filter(avaliacao => !(avaliacao.avaliador === avaliadorToDelete && avaliacao.avaliado === avaliadoToDelete));
+        setAvaliacoes(newAvaliacoes);
+        try {
+            await api.delete(`/api/user/delete/${avaliadorToDelete}/${avaliadoToDelete}/`);
+            window.location.reload();
+        } catch (error) {
+            alert(error);
+        }
+    };
+    return (
+
         <Container fluid className="container-padding-top">
-          <Row>
-              <Col md={2} className="sidebar bg-light">
-                  {/* Sidebar content */}
-              </Col>
-              <Col md={10}>
-                  <h1 className="text-center mb-4 heading-margin-top">Avaliações</h1>
-                  <Table striped bordered hover className="shadow" style={{ margin: '10px', padding: '20px' }}>
-                      <thead className="thead-dark">
-                          <tr>
-                              <th  style={{ margin: '100px', padding: '10px 20px'}}>Avaliador</th>
-                              <th  style={{ margin: '100px', padding: '10px 20px'}}>Avaliados</th>
-                              <th  style={{ margin: '100px', padding: '10px 20px'}}>Data Inicial</th>
-                              <th  style={{ margin: '100px', padding: '10px 20px'}}>Data Final</th>
-                              <th  style={{ margin: '100px', padding: '10px 20px'}}>Actions</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          {Object.entries(avaliadores).map(([avaliador, avaliacoes], index) => (
-                              avaliacoes.map((avaliacao, subIndex) => (
-                                  <tr key={`${index}-${subIndex}`}>
-                                      <td>{avaliador}</td>
-                                      <td>{avaliacao.avaliado}</td>
-                                      <td>{avaliacao.data_inicial}</td>
-                                      <td>{avaliacao.data_final}</td>
-                                      <td>
-                                          <button onClick={() => handleDelete(avaliador, avaliacao.avaliado)} style={{
-                                              background: '#DC0232',
-                                              color: 'white',
-                                              border: 'none',
-                                              padding: '10px 20px',
-                                              margin: '2px',
-                                              cursor: 'pointer',
-                                              borderRadius: '5px',}}>Apagar</button>
-                                          <button onClick={() => navigate('/evaluationForm')} style={{
-                                              background: '#000000',
-                                              color: 'white',
-                                              border: 'none',
-                                              padding: '10px 20px',
-                                              margin: '2px',
-                                              cursor: 'pointer',
-                                              borderRadius: '5px',}}>Ficha</button>
-                                          
-                                      </td>
-                                  </tr>
-                              ))
-                          ))}
-                      </tbody>
-                  </Table>
-              </Col>
-          </Row>
-      </Container>
-  );
+            <Row>
+                <Col md={2} className="sidebar bg-light">
+                    {/* Sidebar content */}
+                </Col>
+                <Col md={10}>
+                    <h1 className="text-center mb-4 heading-margin-top">Avaliações</h1>
+                    <Table striped bordered hover className="shadow" style={{ margin: '10px', padding: '20px' }}>
+                        <thead className="thead-dark">
+                            <tr>
+                                <th style={{ margin: '100px', padding: '10px 20px' }}>Avaliador</th>
+                                <th style={{ margin: '100px', padding: '10px 20px' }}>Avaliados</th>
+                                <th style={{ margin: '100px', padding: '10px 20px' }}>Data Inicial</th>
+                                <th style={{ margin: '100px', padding: '10px 20px' }}>Data Final</th>
+                                <th style={{ margin: '100px', padding: '10px 20px' }}>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Object.entries(avaliadores).map(([avaliador, avaliacoes], index) => (
+                                avaliacoes.map((avaliacao, subIndex) => (
+                                    <tr key={`${index}-${subIndex}`}>
+                                        <td>{avaliador}</td>
+                                        <td>{avaliacao.avaliado}</td>
+                                        <td>{avaliacao.data_inicial}</td>
+                                        <td>{avaliacao.data_final}</td>
+                                        <td>
+                                            <button onClick={() => handleDelete(avaliador, avaliacao.avaliado)} style={{
+                                                background: '#DC0232',
+                                                color: 'white',
+                                                border: 'none',
+                                                padding: '10px 20px',
+                                                margin: '2px',
+                                                cursor: 'pointer',
+                                                borderRadius: '5px',
+                                            }}>Apagar</button>
+                                            <button onClick={() => navigate('/evaluationForm')} style={{
+                                                background: '#000000',
+                                                color: 'white',
+                                                border: 'none',
+                                                padding: '10px 20px',
+                                                margin: '2px',
+                                                cursor: 'pointer',
+                                                borderRadius: '5px',
+                                            }}>Ficha</button>
+
+                                        </td>
+                                    </tr>
+                                ))
+                            ))}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Home
